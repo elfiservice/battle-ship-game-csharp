4d96be8d4cfc79e4e7717f4c602e9774122e6a32
Armando Jr <elfiservice@hotmail.com>
2024-05-13 08:25:30 -0300

    provided Required ships to Game

--- BattleShipGame/Core/Models/Game.cs
+++ BattleShipGame/Core/Models/Game.cs
@@ -1,7 +1,18 @@
+using BattleShipGame.Core.Models.Ships;
+
 namespace BattleShipGame.Core.Models;
 
 public class Game
 {
     Player Player1 { get; set; }
     Player Player2 { get; set; }
+
+    private Ship[] RequiredShips = new Ship[]
+    {
+        new Carrier(),
+        new Battleship(),
+        new PatrolBoat(),
+        new Submarine(),
+        new Destroyer()
+    };
 }
