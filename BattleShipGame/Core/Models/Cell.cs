5146655723efb1ec2e7bb1058fd9186008e7a6c6
Armando Jr <elfiservice@hotmail.com>
2024-05-15 08:03:29 -0300

    refactored Cell class to receive column first

--- BattleShipGame/Core/Models/Cell.cs
+++ BattleShipGame/Core/Models/Cell.cs
@@ -2,7 +2,7 @@ using BattleShipGame.Core.ValuesObjects;
 
 namespace BattleShipGame.Core.Models;
 
-public class Cell(Row row, Column column)
+public class Cell(Column column, Row row)
 {
     public Row Row { get; private set; } = row;
     public Column Column { get; private set; } = column;
