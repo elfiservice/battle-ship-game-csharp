fb28537a5ac406f039d36a0a7c2650bc42088800
Armando Jr <elfiservice@hotmail.com>
2024-05-15 08:02:54 -0300

    turned Player1 public and RequeriedShips public

--- BattleShipGame/Core/Models/Game.cs
+++ BattleShipGame/Core/Models/Game.cs
@@ -4,10 +4,10 @@ namespace BattleShipGame.Core.Models;
 
 public class Game
 {
-    Player Player1 { get; set; }
+    public Player Player1 { get; set; }
     Player Player2 { get; set; }
 
-    private Ship[] RequiredShips = new Ship[]
+    public Ship[] RequiredShips = new Ship[]
     {
         new Carrier(),
         new Battleship(),
