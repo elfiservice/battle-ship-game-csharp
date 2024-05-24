1f7462e8aec78f64ff052a3fa844ea25204b9d02
Armando Jr <elfiservice@hotmail.com>
2024-05-24 08:32:37 -0300

    provided ProvideShipsTo method and show to user the Cell being filled to each ship

--- BattleShipGame/Program.cs
+++ BattleShipGame/Program.cs
@@ -13,48 +13,51 @@ do
     player1Name = Console.ReadLine();
 } while (player1Name is "" or null);
 
-
 game.Player1 = new Player(player1Name, new BattleField());
-var battleFieldPlayer1 = game.Player1.GetBattleField();
+ProvideShipsTo(game.Player1);
 
-var numberOfShipsToProvide = battleFieldPlayer1.RequiredShips.Length;
 
-for (int i = 0; i < numberOfShipsToProvide; i++)
+void ProvideShipsTo(Player player)
 {
-    var ship = battleFieldPlayer1.RequiredShips[i];
-    var shipLength = ship.GetShipLength();
+    var battleFieldPlayer1 = player.GetBattleField();
 
-    do
+    var numberOfShipsToProvide = battleFieldPlayer1.RequiredShips.Length;
+
+    for (int i = 0; i < numberOfShipsToProvide; i++)
     {
-        try
+        var ship = battleFieldPlayer1.RequiredShips[i];
+        var shipLength = ship.GetShipLength();
+
+        do
         {
-            Console.WriteLine($"Player1 {game.Player1.GetUsername()}, Ship{i + 1} {ship.ToString()}: ");
+            try
+            {
+                Console.WriteLine($"Player {player.GetUsername()}, Ship{i + 1} {ship.ToString()}, Cell {shipLength} : ");
         
-            Console.WriteLine("Enter Column: ");
-            var columnValue = Console.ReadLine();
-            var column = new Column(columnValue);
+                Console.WriteLine("Enter Column: ");
+                var columnValue = Console.ReadLine();
+                var column = new Column(columnValue);
         
             
-            Console.WriteLine("Enter Row: ");
-            var rowValue = Console.ReadLine();
-            var row = new Row(Convert.ToInt32(rowValue));
+                Console.WriteLine("Enter Row: ");
+                var rowValue = Console.ReadLine();
+                var row = new Row(Convert.ToInt32(rowValue));
 
-            var cell = new Cell(column, row);
+                var cell = new Cell(column, row);
         
-            ship.FillCell(cell);
+                ship.FillCell(cell);
         
-            --shipLength;
-        }
-        catch (Exception e)
-        {
-            Console.WriteLine($"Ops! {e.Message}, try again.");
-        }
+                --shipLength;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"Ops! {e.Message}, try again.");
+            }
 
-    } while (shipLength > 0);
+        } while (shipLength > 0);
     
-    Console.WriteLine($"LOG: {ship.ShowShipToString()}");
+        Console.WriteLine($"LOG: {ship.ShowShipToString()}");
+    }
 }
-
-
 
 Console.WriteLine("##### END #####");
