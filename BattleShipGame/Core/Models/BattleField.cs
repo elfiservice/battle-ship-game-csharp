a64717f52b7d317bb9d73bf43a439285274b871e
Armando Jr <elfiservice@hotmail.com>
2024-06-30 10:28:08 -0300

    provided ShipOverlayBoundaries method to BattleField.cs

--- BattleShipGame/Core/Models/BattleField.cs
+++ BattleShipGame/Core/Models/BattleField.cs
@@ -26,4 +26,47 @@ public class BattleField
                     requiredShip.GetCells().Any(cell =>
                         cell.GetCordinates() == cellCordinates));
     }
+    
+    public bool ShipOverlayBoundaries(Ship ship)
+    {
+        var dimension = GetDimension();
+        var shipLength = ship.GetShipLength();
+        var shipDirection = ship.GetDirection();
+
+        var minimumGap = dimension - shipLength;
+        var firstCellOfShipExpected = minimumGap + 1;
+
+        var shipCells = ship.GetCells();
+        
+        if (shipCells.Length == 0)
+        {
+            return false;
+        }
+        
+        var firstCell = ship.GetCells().First();
+
+        if (shipDirection == 1)
+        {
+            // Horizontal
+            var firstColumn = firstCell.Column.GetValue();
+            var actualFirstColumnPosition = Array.IndexOf(_validDimension, firstColumn) + 1;
+
+            if (actualFirstColumnPosition > firstCellOfShipExpected)
+            {
+                return true;
+            }
+        }
+        else
+        {
+            // Vertical
+            var actualFirstRowPosition = firstCell.Row.GetValue();
+
+            if (actualFirstRowPosition > firstCellOfShipExpected)
+            {
+                return true;
+            }
+        }
+
+        return false;
+    }
 }
